b9a1c921022b1f3c3646d6ef59417e53ae33bdcd
YutaNiitsu <yniitsu@air.ocn.ne.jp>
2023-10-26 11:32:20 +0900

    フォルダ追加

---
+++ Assets/Common/Test/ButtanTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtanTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Press1()
+    {
+        Debug.Log("Buttan1");
+    }
+    public void Press2()
+    {
+        Debug.Log("Buttan2");
+    }
+    public void Press3()
+    {
+        Debug.Log("Buttan3");
+    }
+}
