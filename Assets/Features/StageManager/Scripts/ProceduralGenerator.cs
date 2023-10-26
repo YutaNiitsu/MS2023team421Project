be6927034be6dabde4dafca1423dcba8b08041d8
YutaNiitsu <yniitsu@air.ocn.ne.jp>
2023-10-26 14:26:40 +0900

    ステージマネージャーのスクリプト追加

---
+++ Assets/Features/StageManager/Scripts/ProceduralGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+// ゲーム開始時に星とそれをはめ込む場所を配置するシステム
+public class ProceduralGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
