e27f62f43fbe36b3a8bcb8ef870d4b47cc94cae7
YutaNiitsu <yniitsu@air.ocn.ne.jp>
2023-10-27 17:52:56 +0900

    星はめ込む型作成

---
+++ Assets/Features/Target/Scripts/TargetScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject obj = collision.gameObject;
+
+        // 星だった時
+        if (obj.CompareTag("Star"))
+        {
+            // 位置をターゲットに合わせる
+            Vector3 pos = gameObject.transform.position;
+            obj.transform.position = pos;
+
+            Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                // 速度を０にする
+                rb.velocity = Vector3.zero;
+            }
+        }
+    }
+}
