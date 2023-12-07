bbe95abedd39951ed4d63cb32b5d515d3c68f131
YutaNiitsu <yniitsu@air.ocn.ne.jp>
2023-12-07 12:19:04 +0900

    動かない障害物の画像アタッチ

--- Assets/Features/Gimmick/Scripts/ObstacleScript.cs
+++ Assets/Features/Gimmick/Scripts/ObstacleScript.cs
@@ -6,11 +6,15 @@ public class ObstacleScript : MonoBehaviour
 {
     [Header("消滅自のパーティクルのプレハブ")]
     public ParticleSystem Particle;
+    public Sprite[] Sprites;
 
+    private SpriteRenderer Sprite;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Sprite = GetComponent<SpriteRenderer>();
+        int rand = UnityEngine.Random.Range(0, Sprites.Length - 1);
+        Sprite.sprite = Sprites[rand];
     }
 
     public void OnDestroy()
