d31afe2aee678da9ab8c45ee7b632dd2454e5c8e
YutaNiitsu <yniitsu@air.ocn.ne.jp>
2023-12-15 15:29:36 +0900

    生成する星座の設定

---
+++ Assets/Features/UI/Scripts/TutorialUIScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialUIScript : MonoBehaviour
+{
+    public GameObject[] OperatingTutorial;
+    public GameObject[] StarTypeTutorial;
+    public GameObject[] ObstacleTutorial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
