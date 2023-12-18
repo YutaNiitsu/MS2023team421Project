b883620110e78fec496dc03d60a151348ae799e7
yuta nakamoto <atgc7828@gmail.com>
2023-12-19 01:19:43 +0900

    タイトル(クレジット以外完成) ステージセレクト(途中)

---
+++ Assets/Features/StageSelect/Stageselect.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+
+public class Stageselect : MonoBehaviour
+{
+
+    //public GameObject SelectCam;
+
+    //public GameObject[] right;
+    //public GameObject[] left;
+    //public GameObject[] down;
+    //public GameObject[] up;
+    //public GameObject[] back;
+
+
+    //// Use this for initialization
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Camera cam = SelectCam.GetComponent();
+
+    //    Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    //    Collider2D col = Physics2D.OverlapPoint(mousePos);
+
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        //右に移動
+    //        foreach (GameObject Right in right)
+    //        {
+    //            if (col == Right.GetComponent())
+    //            {
+    //                SelectCam.transform.position = new Vector3(this.transform.position.x + 40, this.transform.position.y, -10);
+    //            }
+    //        }
+    //        //左に移動
+    //        foreach (GameObject Left in left)
+    //        {
+    //            if (col == Left.GetComponent())
+    //            {
+    //                SelectCam.transform.position = new Vector3(this.transform.position.x - 40, this.transform.position.y, -10);
+    //            }
+    //        }
+    //        //上に移動
+    //        foreach (GameObject UP in up)
+    //        {
+    //            if (col == UP.GetComponent())
+    //            {
+    //                SelectCam.transform.position = new Vector3(0, 0, -10);
+    //            }
+    //        }
+    //        //下に移動
+    //        foreach (GameObject Down in down)
+    //        {
+    //            if (col == Down.GetComponent())
+    //            {
+    //                SelectCam.transform.position = new Vector3(0, -20, -10);
+    //            }
+    //        }
+    //        //タイトルへ戻る
+    //        foreach (GameObject Back in back)
+    //        {
+    //            if (col == Back.GetComponent())
+    //            {
+    //                Application.LoadLevel("0");
+    //                //Scene0 = Title.name
+    //            }
+    //        }
+    //    }
+    }
